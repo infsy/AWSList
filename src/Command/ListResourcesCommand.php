0ca6701e85a7326c3dcb4e588a1ebf137e97f72c
cedric <cedric@www.cedricthibault.fr>
2019-03-13 11:37:15 -0400

    Inclusion export XLS

--- src/Command/ListResourcesCommand.php
+++ src/Command/ListResourcesCommand.php
@@ -5,10 +5,12 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Aws\ConfigService\ConfigServiceClient;
 use Aws\Ec2\Ec2Client;
 use App\Entity\AWSObject;
+use Yectep\PhpSpreadsheetBundle\Factory;
 
 class ListResourcesCommand extends ContainerAwareCommand
 {
@@ -18,11 +20,19 @@ class ListResourcesCommand extends ContainerAwareCommand
 	{
 		$this
 			->setName('app:list:resources')
-			->setDescription('This list all of your AWS ressources');
+			->setDescription('This list all of your AWS ressources')
+			->addOption(
+        			'export',
+        			null,
+        			InputOption::VALUE_OPTIONAL,
+        			'If you want a CSV Export of the list',
+        			false
+    		);
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
+		
 		// initializing Container to work with Entities
 		$em = $this->getContainer()->get('doctrine')->getManager();
 		$em->getConnection()->getConfiguration()->setSQLLogger(null);
@@ -42,9 +52,24 @@ class ListResourcesCommand extends ContainerAwareCommand
 		//
 		$output->writeln('Type of object checked : ');
 		print_r($listResources);	
+
+
+// https://docs.aws.amazon.com/aws-sdk-php/v2/api/class-Aws.Sts.StsClient.html#_getFederationToken
+$result = $client->assumeRole(array(
+    // RoleArn is required
+    'RoleArn' => 'arn:aws:iam::836114456886:role/ADFS-CAAProdWeb-Audit',
+    // RoleSessionName is required
+    'RoleSessionName' => 'cedric.thibault3@caaquebec.com',
+    // 'Policy' => 'string',
+    // 'DurationSeconds' => integer,
+    'ExternalId' => 'cedric.thibault3@caaquebec.com',
+    // 'SerialNumber' => 'string',
+    // 'TokenCode' => 'string',
+));
+
 		// Get list of AWS Regions through EC2 describeRegions command
 		$ec2client = Ec2Client::factory(array(
-			'profile' => 'awslabs1',
+			'profile' => 'default',
 			'version' => '2016-04-01',
 			'region' => 'us-east-1'
 		));
@@ -55,7 +80,7 @@ class ListResourcesCommand extends ContainerAwareCommand
 
 			// Check if AWSConfig service is enabled for recording resources
 			$client = ConfigServiceClient::factory(array(
-				'profile' => 'awslabs1',
+				'profile' => 'default',
 				'version' => '2014-11-12',
 				'region' => $region['RegionName']
 			));
@@ -102,6 +127,7 @@ class ListResourcesCommand extends ContainerAwareCommand
 						$object->setAWSLastDetection(new \DateTime());
 						$em->persist($object);
 						$em->flush();
+						
 					} // End of foreach resource of a type recorded in the Region
 				} // End of foreach ResourceType loop
 
@@ -114,6 +140,49 @@ class ListResourcesCommand extends ContainerAwareCommand
 				} // End of the loop to retrieve the count for each resource recorded and checked
 			} // End of condition if Config is recording in the Region
 		} //End of foreach Region loop
+		
+		// checking if need of csv export 
+		$optionValue = $input->getOption('export');
+		if ($optionValue) { 
+			writeln('on exporte');
+			$this->export();
+		}
+	}
+	public function export(Factory $factory)
+	{
+		$spreadsheet = $factory->createSpreadsheet();
+			// Set document properties
+			$spreadsheet->getProperties()->setCreator('CAA')
+				 ->setLastModifiedBy('CAA')
+    			->setTitle('Scan result export')
+    			->setSubject('Office 2007 XLSX Test Document')
+    			->setDescription('Export of AWS objects found.')
+    			->setKeywords('office 2007 openxml php')
+    			->setCategory('Test result file');
+    		$sheet = $spreadsheet->getActiveSheet();
+			$sheet->setTitle('AWS Objects');
+			$sheet->setCellValue('A1', 'List of objects from AWS default access keys');
+			$ligne = 3;
+			
+			$objects = $em->getRepository('App:AWSObject')->findAll();
+			foreach ($objects as $AWSObject) {
+							$sheet->setCellValue('A'.$ligne, $AWSObject->getAWSId());
+							$sheet->setCellValue('B'.$ligne, $AWSObject->getAWSType());
+							if (!empty($AWSObject->getAWSName())) {
+								$sheet->setCellValue('C'.$ligne, $objects->getAWSName());
+							}
+							if (!empty($AWSObject->getAWSDeletionTime())) {
+								$sheet->setCellValue('D'.$ligne, $objects->getAWSDeletionTime());
+							}
+							if (!empty($AWSObject->getAWSRegionName())) {
+								$sheet->setCellValue('E'.$ligne, $objects->getAWSRegionName());
+							}
+							
+							$ligne++; 
+				
+			}
+		$writerXlsx = $this->get('phpoffice.spreadsheet')->createWriter($spreadsheet, 'Xlsx');
+		$writerXlsx->save('./exportAWSObjects.xlsx');
 	}
 }
 
